fb5ed72177ac76fcc2d4f6bdf6b70a4ac1e4f10f
silverninja123 <goldencrumbs27@gmail.com>
2026-02-22 21:27:44 +0500

    Add files via upload

---
+++ index6.py
@@ -0,0 +1,55 @@
+alphabet=input("Write any alphabet or number :")
+if alphabet=="a":
+    print("The input is a alphabet.")
+elif alphabet=="b":
+    print("The input is a alphabet.")
+elif alphabet=="c":
+    print("The input is a alphabet.")
+elif alphabet=="d":
+    print("The input is a alphabet.")
+elif alphabet=="e":
+    print("The input is a alphabet.")
+elif alphabet=="f":
+    print("The input is a alphabet.")
+elif alphabet=="g":
+    print("The input is a alphabet.")
+elif alphabet=="h":
+    print("The input is a alphabet.")
+elif alphabet=="i":
+    print("The input is a alphabet.")
+elif alphabet=="j":
+    print("The input is a alphabet.")
+elif alphabet=="k":
+    print("The input is a alphabet.")
+elif alphabet=="l":
+    print("The input is a alphabet.")
+elif alphabet=="m":
+    print("The input is a alphabet.")
+elif alphabet=="n":
+    print("The input is a alphabet.")
+elif alphabet=="o":
+    print("The input is a alphabet.")
+elif alphabet=="p":
+    print("The input is a alphabet.")
+elif alphabet=="q":
+    print("The input is a alphabet.")
+elif alphabet=="r":
+    print("The input is a alphabet.")
+elif alphabet=="s":
+    print("The input is a alphabet.")
+elif alphabet=="t":
+    print("The input is a alphabet.")
+elif alphabet=="u":
+    print("The input is a alphabet.")
+elif alphabet=="v":
+    print("The input is a alphabet.")
+elif alphabet=="w":
+    print("The input is a alphabet.")
+elif alphabet=="x":
+    print("The input is a alphabet.")
+elif alphabet=="y":
+    print("The input is a alphabet.")
+elif alphabet=="z":
+    print("The input is a alphabet.")
+else:
+    print("The input is a number.")
